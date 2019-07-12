849529cbc94b9c851cdfd1feafe257d7d5443517
przekwas <luke@covalence.io>
2019-07-11 23:21:14 -0500

    adding double join to knex query

--- server/db/queries/games.js
+++ server/db/queries/games.js
@@ -1,6 +1,13 @@
 import { connection as knex } from '../index';
 
-const gamesForWeek = weekid => knex('games').select().where('weekid', weekid);
+const gamesForWeek = weekid => {
+    return knex('games').select('games.weekid', 'teams.id as teamid', 'teams.name').join('teams', function() {
+        this
+            .on('teams.id', '=', 'games.home_team')
+            .orOn('teams.id', '=', 'games.away_team')
+    }).where('games.weekid', weekid);
+}
+
 const insert = query => knex('games').insert(query);
 
 export default {
